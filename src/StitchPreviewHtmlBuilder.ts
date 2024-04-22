7b04c14745db14d56d52898281c333622a394222
Bram van der Horn <bvanderhorn@gmail.com>
2024-04-22 16:18:48 +0200

    feat: validate format and show functional error message for response (#55)

--- src/StitchPreviewHtmlBuilder.ts
+++ src/StitchPreviewHtmlBuilder.ts
@@ -83,17 +83,21 @@ function _createStepsHtml(response: EditorSimulateIntegrationResponse): string {
 
 function _createResponseHtml(response: EditorSimulateIntegrationResponse): string {
     const actionCommand = `{action: ${CommandAction.viewIntegrationResponse} }`;
-
-
-    let body = `<pre><code>${_getResponseBody(response.result)}</code></pre>`;
+    
+    let body = ``;
 
     if (response.result.headers) {
-        body = `<p>${Object.keys(response.result.headers).map(key => `${key}:&nbsp;${response.result.headers?.[key]}<br />`).join('')}</p>
-                ${body}`;
+        body += `<p>${Object.keys(response.result.headers).map(key => `${key}:&nbsp;${response.result.headers?.[key]}<br />`).join('')}</p>`;
     }
 
+    if (response.validFormat === false) {
+        body += `<p>Format error:&nbsp;&nbsp;<span class="errormessage">${response.formatErrorMessage}</span></p>`;
+    }
+
+    body += `<pre><code>${_getResponseBody(response.result)}</code></pre>`;
+
     return `<h2 id="integration_response">Response</h2>
-            ${_createActionHtml(`${response.result.statusCode}`, response.result.outputType, '', actionCommand, body)}`;
+            ${_createActionHtml(`${response.result.statusCode}`, response.result.outputType, '', actionCommand, body, (response.validFormat ?? true) ? 'action' : 'actionerror')}`;
 }
 
 function _createNavHtml(steps: Record<string, StepResult>): string {
@@ -179,11 +183,12 @@ function _getHttpStepHtml(configuration: HttpStepConfiguration) {
     if (configuration.headers) {
         html += `<p>${Object.keys(configuration.headers).map(key => `${key}:&nbsp;${configuration.headers?.[key]}<br />`).join('')}</p>`;
     }
-    html += `<p>
-                Valid format:&nbsp;&nbsp;${configuration.validFormat}<br/>`;
+    html += `<p>`;
+
     if (configuration.validFormat === false) {
         html += `Format error:&nbsp;&nbsp;<span class="errormessage">${configuration.formatErrorMessage}</span><br />`;
     }
+
     html +=`Encoding name:&nbsp;${configuration.encodingName}
             </p>`;
     html += `<button class="file-btn" onclick="vscode.postMessage({action: ${CommandAction.createHttpRequest}, content: '${configuration.id}' });">Create HTTP request</button>`;
